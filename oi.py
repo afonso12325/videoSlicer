43fe57927f7e8ac62007c57578a2545de3bd7dbc
afonso <afonso12325>
2019-05-11 23:09:06 -0300

    yolo

--- oi.py
+++ oi.py
@@ -1,4 +1,16 @@
 from kerasyolo3.yolo import YOLO
 import cv2
+from PIL import Image
+import numpy as np
 
 yolo_model = YOLO()
+img1 = cv2.imread("videos/mehdi.png")
+img2 = Image.fromarray(img1)
+img2 = yolo_model.detect_image(img2)
+img2 = np.asarray(img2)
+while(True):
+    cv2.imshow("mehdi", img1)
+    cv2.imshow("yolo", img2)
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
+cv2.destroyAllWindows()
