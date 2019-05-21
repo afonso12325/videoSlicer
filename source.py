7d9f3deea974350658604fc79128614839aedbba
afonso <afonso12325>
2019-05-21 07:52:43 -0300

    debugging

--- source.py
+++ source.py
@@ -8,7 +8,8 @@ from neuralstyle.stylize import stylize
 from skimage import img_as_ubyte
 import tensorflow as tf
 
-sess = tf.Session()
+nst_flag = True
+nst_frame = None
 
 def split(img, n):
     sectors = []
@@ -38,72 +39,85 @@ def apply_single_transform(sector,transform, objects):
         img2 = model.detect_image(img2)
         return np.asarray(img2)
     if transform == 'nst':
-        # default arguments
-        global sess
-        CONTENT_WEIGHT = 5e0
-        CONTENT_WEIGHT_BLEND = 1
-        STYLE_WEIGHT = 5e2
-        TV_WEIGHT = 1e2
-        STYLE_LAYER_WEIGHT_EXP = 1
-        LEARNING_RATE = 1e1
-        BETA1 = 0.9
-        BETA2 = 0.999
-        EPSILON = 1e-08
-        STYLE_SCALE = 1.0
-        ITERATIONS = 200
-        VGG_PATH = 'imagenet-vgg-verydeep-19.mat'
-        POOLING = 'max'
-        resize_fac = 0.3
-        prev_shape = sector.shape[:2]
-        content_image = cv2.resize(sector,(int(sector.shape[1]*resize_fac),int(sector.shape[0]*resize_fac)))
-        style_images = [cv2.imread('videos/1-style.jpg'), ]
-        initial = content_image
-        initial_noiseblend = 0
-        preserve_colors = None
-        iterations = ITERATIONS
-        content_weight = CONTENT_WEIGHT
-        content_weight_blend = CONTENT_WEIGHT_BLEND
-        style_weight = STYLE_WEIGHT
-        style_layer_weight_exp = STYLE_LAYER_WEIGHT_EXP
-        style_blend_weights = [1,]
-        tv_weight = TV_WEIGHT
-        learning_rate = LEARNING_RATE
-        beta1 = BETA1
-        beta2 = BETA2
-        epsilon = EPSILON
-        pooling = POOLING
-        print_iterations= None
-        checkpoint_iterations = None
-        for iteration, image, loss_vals in stylize(
-                                            network=VGG_PATH,
-                                            initial=initial,
-                                            initial_noiseblend=initial_noiseblend,
-                                            content=content_image,
-                                            styles=style_images,
-                                            preserve_colors=preserve_colors,
-                                            iterations=iterations,
-                                            content_weight=content_weight,
-                                            content_weight_blend=content_weight_blend,
-                                            style_weight=style_weight,
-                                            style_layer_weight_exp=style_layer_weight_exp,
-                                            style_blend_weights=style_blend_weights,
-                                            tv_weight=tv_weight,
-                                            learning_rate=learning_rate,
-                                            beta1=beta1,
-                                            beta2=beta2,
-                                            epsilon=epsilon,
-                                            pooling=pooling,
-                                            print_iterations=print_iterations,
-                                            checkpoint_iterations=checkpoint_iterations,
-        ):
-            pass
-        min_v = np.min(image)
-        image = image-min_v #to have only positive values
-        max_v=np.max(image) 
-        div=max_v/255.0 #calculate the normalize divisor
-        image=np.uint8(image/div)
-        cv2.imwrite("out700.png", cv2.resize(image, (prev_shape[1], prev_shape[0])))
-        return cv2.resize(image, (prev_shape[1], prev_shape[0]))
+        global nst_flag
+        global nst_frame
+
+        left_upper = (0.1,0.1)
+        right_bottom = (0.4,0.4)
+        if nst_flag:
+            # default arguments
+            CONTENT_WEIGHT = 5e0
+            CONTENT_WEIGHT_BLEND = 1
+            STYLE_WEIGHT = 5e2
+            TV_WEIGHT = 1e2
+            STYLE_LAYER_WEIGHT_EXP = 1
+            LEARNING_RATE = 1e1
+            BETA1 = 0.9
+            BETA2 = 0.999
+            EPSILON = 1e-08
+            STYLE_SCALE = 1.0
+            ITERATIONS = 200
+            VGG_PATH = 'imagenet-vgg-verydeep-19.mat'
+            POOLING = 'max'
+            resize_fac = 0.3
+            prev_shape = sector.shape[:2]
+            content_image = cv2.resize(sector,(int(sector.shape[1]*resize_fac),int(sector.shape[0]*resize_fac)))
+            style_images = [cv2.imread('videos/1-style.jpg'), ]
+            initial = content_image
+            initial_noiseblend = 0
+            preserve_colors = None
+            iterations = ITERATIONS
+            content_weight = CONTENT_WEIGHT
+            content_weight_blend = CONTENT_WEIGHT_BLEND
+            style_weight = STYLE_WEIGHT
+            style_layer_weight_exp = STYLE_LAYER_WEIGHT_EXP
+            style_blend_weights = [1,]
+            tv_weight = TV_WEIGHT
+            learning_rate = LEARNING_RATE
+            beta1 = BETA1
+            beta2 = BETA2
+            epsilon = EPSILON
+            pooling = POOLING
+            print_iterations= None
+            checkpoint_iterations = None
+            for iteration, image, loss_vals in stylize(
+                                                network=VGG_PATH,
+                                                initial=initial,
+                                                initial_noiseblend=initial_noiseblend,
+                                                content=content_image,
+                                                styles=style_images,
+                                                preserve_colors=preserve_colors,
+                                                iterations=iterations,
+                                                content_weight=content_weight,
+                                                content_weight_blend=content_weight_blend,
+                                                style_weight=style_weight,
+                                                style_layer_weight_exp=style_layer_weight_exp,
+                                                style_blend_weights=style_blend_weights,
+                                                tv_weight=tv_weight,
+                                                learning_rate=learning_rate,
+                                                beta1=beta1,
+                                                beta2=beta2,
+                                                epsilon=epsilon,
+                                                pooling=pooling,
+                                                print_iterations=print_iterations,
+                                                checkpoint_iterations=checkpoint_iterations,
+            ):
+                pass
+            min_v = np.min(image)
+            image = image-min_v #to have only positive values
+            max_v=np.max(image) 
+            div=max_v/255.0 #calculate the normalize divisor
+            image=np.uint8(image/div)
+            nst_frame = cv2.resize(image, (prev_shape[1], prev_shape[0]))
+            nst_flag = False
+            return cv2.resize(image, (prev_shape[1], prev_shape[0]))
+        else:
+            dur = objects['dur']
+            lu_rel = np.array(left_upper)*dur
+            rb_rel = 1 - np.array(right_bottom)*dur
+            pixels_lu = (lu_rel*np.array(sector.shape[0:2:-1])).astype(np.uint8)
+            pixels_rb = (lu_rel*np.array(sector.shape[0:2:-1])).astype(np.uint8)
+            sector[pixels_lu[1]:pixels_rb[1],pixels_lu[0]:pixels_rb[0], :] = cv2.resize(nst_frame, (pixels_rb[0]-pixels_lu[0], pixels_rb[1]-pixels_lu[1]))
 
         
 def apply_transforms(frame, keep_orig, transforms, objects):
@@ -127,7 +141,7 @@ def check_weights():
 if __name__ == "__main__":
     parameters = [
         #{'duration':(1, 400), 'keep_orig':True, 'transforms':['b&w', 'canny'], 'transform_objects':{}},
-        {'duration':(300, 400), 'keep_orig':False, 'transforms':['nst'], 'transform_objects':{}},
+        {'duration':(300, 800), 'keep_orig':False, 'transforms':['nst'], 'transform_objects':{"dur": 0}},
         #{'duration':(700, 1200), 'keep_orig':False, 'transforms':['canny', 'const'], 'transform_objects':{}},
     ]
     check_weights()
@@ -166,6 +180,7 @@ if __name__ == "__main__":
 
             for parameter in parameters:
                 if frame_count>=parameter['duration'][0] and frame_count<=parameter['duration'][1]:
+                    parameter['transform_objects']["dur"] = float(frame_count - parameter['duration'][0])/float(frame_count<=parameter['duration'][1] -frame_count>=parameter['duration'][0])
                     frame = apply_transforms(frame, keep_orig = parameter['keep_orig'], transforms = parameter['transforms'], objects = parameter['transform_objects'])
                     
             
