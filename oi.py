aa3c1bd0304927ed0bf755c22cba32c3de43ce7e
afonso <afonso12325>
2019-05-21 00:00:09 -0300

    trying neural art trasnfer

--- oi.py
+++ oi.py
@@ -3,14 +3,27 @@ import cv2
 from PIL import Image
 import numpy as np
 
-yolo_model = YOLO()
-img1 = cv2.imread("videos/im100.jpg")
-img2 = Image.fromarray(img1)
-img2 = yolo_model.detect_image(img2)
-img2 = np.asarray(img2)
+
+cap = cv2.VideoCapture('videos/test.mp4')
+frame_count = 0
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)   # float
+height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT) # float
+fps = cap.get(cv2.CAP_PROP_FPS)
+print('width = {} height = {} fps = {}'.format(width, height, fps))
+out = cv2.VideoWriter('out/out_curto.avi',fourcc, 23.98, (1280,720))
 while(True):
-    cv2.imshow("mehdi", img1)
-    cv2.imshow("yolo", img2)
-    if cv2.waitKey(20) & 0xFF == ord('q'):
-        break
+	ret, frame = cap.read()
+	if ret:
+            
+		frame_count += 1
+		#if(frame_count%100 == 0):
+		print(frame_count)
+		cv2.imshow('frame',frame)
+		if 70<=frame_count<=73:
+    			out.write(frame)
+	if cv2.waitKey(20) & 0xFF == ord('q'):
+		break
+cap.release()
+out.release()
 cv2.destroyAllWindows()
